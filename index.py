3dc208e410d6e6f1ba649e02e0c9ad39d4e6eb2a
argg9822 <argg9822@gmail.com>
2024-03-17 21:34:56 -0500

    Menu bases

--- index.py
+++ index.py
@@ -2,8 +2,10 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import tkinter.font as font
 import subprocess
+from validadores import institucional
 
 fontTitle = ("Helvetica", 30, "bold")
+fontTexts = ("Helvetica", 15)
 principalColor = "#191919"
 themeColor = "#F15025"
 secondColor = "#36373B"
@@ -22,9 +24,10 @@ def ajustar_ancho_menu(event):
     ancho_marco_menu = int(event.width * 0.3)  # 10% del ancho de la ventana
     marco_menu.config(width=ancho_marco_menu)
 
-def ejecutarValidadorEntornos(script_path):
+def ejecutarValidadorEntornos(script_path, base):
     def callback():
-        subprocess.Popen(["python", script_path])  # Ejecutar el script cuando se hace clic en el botón
+        subprocess.Popen(["python", 'validadores/'+script_path+'.py', base])  # Ejecutar el script cuando se hace clic en el botón
+        institucional.setBase(base)
     return callback
 
 def mostrarBases(entorno):
@@ -37,46 +40,72 @@ def mostrarBases(entorno):
             "institucional": ["sesiones_colectivas", "ead", "hcb", "mascota_verde", "ipa", "pcb", "persona_mayor", "pci", "tamizajes"]
         }
 
+        #Vaciar contenedor
+        for element in marco_botones_bases.winfo_children():
+            element.destroy()
+
         found = False
+        botones = []
         for key, value in bases.items():
-            print("Entró")
             if key == entorno.lower():
-                print(f"Match found in category '{key}':")
-                print(value)
                 found = True
+                for v in value:
+                    btn_bases = tk.Button(marco_botones_bases, text=v,  width="20", height="2", borderwidth=2, highlightbackground="white", bg=themeColor, fg="white", command=ejecutarValidadorEntornos(entorno.lower(), v))
+                    botones.append(btn_bases)
+
+                for index, boton in enumerate(botones):
+                    row = index // 5
+                    col = index % 5
+                    boton.grid(row=row, column=col, padx="5", pady=5, ipadx="2")
                 break
         if not found:
             print("No match found.")
     return callback
 
-# Crear la interfaz gráfica
-root = tk.Tk()
-root.title("Validador GesiApp")
+def buildGUI ():    
+    # Crear la interfaz gráfica
+    root = tk.Tk()
+    root.title("Validador GesiApp")
 
-# Calcular las dimensiones de la ventana
-calcular_dimensiones_ventana(root)
+    # Calcular las dimensiones de la ventana
+    calcular_dimensiones_ventana(root)
 
-# Título en la parte superior
-titulo_label = tk.Label(root, text="Validador GesiApp", font=fontTitle, bg=principalColor, fg=themeColor, pady="20")
-titulo_label.pack(side="top", fill="x")
+    # Título en la parte superior
+    titulo_label = tk.Label(root, text="Validador GesiApp", font=fontTitle, bg=principalColor, fg=themeColor, pady="20")
+    titulo_label.pack(side="top", fill="x")
 
-# Marco principal
-marco_principal = tk.Frame(root, bg=principalColor, padx="10")
-marco_principal.pack(expand=True, fill="both")
+    # Marco principal
+    marco_principal = tk.Frame(root, bg=principalColor, padx="10")
+    marco_principal.pack(expand=True, fill="both")
 
-# Marco para el menú en la parte izquierda (10% del ancho)
-marco_menu = tk.Frame(marco_principal, bg=secondColor)
-marco_menu.pack(side="left", fill="y", pady=(0, 30))
+    # Marco para el menú en la parte izquierda (10% del ancho)
+    global marco_menu 
+    marco_menu = tk.Frame(marco_principal, bg=secondColor)
+    marco_menu.pack(side="left", fill="y", pady=(0, 30))
 
-# Marco para los botones en la parte derecha (90% del ancho)
-marco_botones = tk.Frame(marco_menu, bg=secondColor, width=root.winfo_width() * 0.3, padx="50", pady="30")
-marco_botones.pack(side="left", expand=True, fill="y")
+    # Marco para los botones en la parte derecha (90% del ancho)
+    marco_botones_entornos = tk.Frame(marco_menu, bg=secondColor, width=root.winfo_width() * 0.3, padx="50", pady="30")
+    marco_botones_entornos.pack(side="left", expand=True, fill="y")
 
-# Botones entornos
-entornos = ["Hogar", "Laboral", "Educativo", "Comunitario", "Institucional"]
+    altura_marco_btnBases = int(marco_principal.winfo_height() * 0.20)
+    global marco_botones_bases
+    marco_botones_bases = tk.Frame(marco_principal, bg=secondColor, pady="20", padx="10", height=altura_marco_btnBases)
+    marco_botones_bases.pack(side="right",expand=True, fill="x")
+    marco_botones_bases.place(relx=0.6, rely=0, relwidth=0.7, relheight=0.5, anchor="n")
 
-for entorno in entornos:
-    btn_entorno = tk.Button(marco_botones, text=entorno,  width="20", height="3", borderwidth=2, highlightbackground="white", bg=themeColor, fg="white", command=mostrarBases(entorno))
-    btn_entorno.pack(pady=20)
+    preview_message = tk.Message(marco_botones_bases, text="Por favor seleccione un entorno", fg="white", bg=secondColor, font=fontTexts, borderwidth=2, relief="solid", highlightbackground=themeColor)
+    preview_message.pack(fill="both", expand=True, anchor="center", padx="20", pady="20")
 
-root.mainloop()
+    marco_resultado = tk.Frame(marco_principal, bg=secondColor, pady="20")
+    marco_resultado.pack(side="bottom",expand=True, fill="x")
+
+    # Botones entornos
+    entornos = ["Hogar", "Laboral", "Educativo", "Comunitario", "Institucional"]
+
+    for entorno in entornos:
+        btn_entorno = tk.Button(marco_botones_entornos, text=entorno,  width="20", height="3", borderwidth=2, highlightbackground="white", bg=themeColor, fg="white", command=mostrarBases(entorno))
+        btn_entorno.pack(pady=20)
+
+    root.mainloop()
+    
+buildGUI()
--- validadores/institucional.py
+++ validadores/institucional.py
@@ -1,39 +1,80 @@
+import shutil
 from openpyxl import load_workbook
+import openpyxl
 from openpyxl.styles import PatternFill
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from io import BytesIO
 
-# Cargar el archivo Excel
-wb = load_workbook('SC.xlsx')
+bgError = '#FF0000'
 
-hoja = wb.get_sheet_by_name("Institucion")
-bgError = PatternFill(start_color='FF0000', end_color='FF0000', fill_type='solid')
+def loadExcel ():
+    global fileRoute
+    fileRoute = filedialog.askopenfilename(filetypes=[("Archivos Excel", "*.xlsx;*.xls")])
+    if fileRoute:
+        global df
+        df = pd.read_excel(fileRoute, sheet_name=0, header=1)
+
+def setBase(base):
+    loadExcel()
+    chooseBase(base)
     
 def validarTelefono ():
-    for fila in hoja.iter_rows(min_row=3, max_row=hoja.max_row, min_col=33, max_col=33, values_only=False):
+    for index, fila in df.iterrows():
         # Agrega el valor de la celda en la columna A a la lista
-        cellTelefono = fila[0]
-
+        cellTelefono = fila['Telefono']
         # Agrega el valor convertido a la lista de valores enteros
-        if len(str(cellTelefono.value)) != 7 and len(str(cellTelefono.value)) != 10:
-            cellTelefono.fill = bgError
-            cellFicha = hoja.cell(cellTelefono.row, 2)
-            cellFicha.fill = bgError
+        if len(str(cellTelefono).strip()) != 7 and len(str(cellTelefono).strip()) != 10:
+            df['Telefono'] = df['Telefono'].astype('object')
+            df.at[index, 'Telefono'] = '<span style="color: {};">{}</span>'.format(bgError, cellTelefono)
+            # cellTelefono.fill = bgError
+            # cellFicha = df.cell(cellTelefono.row, 2)
+            # cellFicha.fill = bgError
     
     return True
 
 def validarNoManzana ():
-    for fila in hoja.iter_rows(min_row=3, max_row=hoja.max_row, min_col=36, max_col=36, values_only=False):
-        cellManzana = fila[0]
-        nroManzana = hoja.cell(cellManzana.row, 37)        
+    for index, fila in df.iterrows():
+        cellManzana = fila['.Manzana de cuidado.']
+        nroManzana = fila['.Nro Manzana.']        
 
-        if cellManzana.value == "SI" and nroManzana.value is None:            
-            nroManzana.fill = bgError
-            cellFicha = hoja.cell(cellManzana.row, 2)
-            cellFicha.fill = bgError
+        if cellManzana == "SI" and nroManzana is None:    
+            print("Error en manzana")        
+            # nroManzana.fill = bgError
+            # cellFicha = df.cell(cellManzana.row, 2)
+            # cellFicha.fill = bgError
 
     return True
 
-validarNoManzana()
-validarTelefono()
+def sc():
+    validarTelefono()
+    validarNoManzana()
+    
+def chooseBase(base):
+    swicth = {
+        "sesiones_colectivas":sc
+    }
 
-# Guardar archivo
-wb.save('SC.xlsx')
+    ejexute_validator = swicth.get(base)
+    ejexute_validator()
+    saveFile()
+
+
+def saveFile():
+    try:
+        file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
+        if not file_path:
+            return
+        if not file_path.endswith('.xlsx'):
+            file_path += '.xlsx'
+        file_path_modificado = file_path.replace('.xlsx', '_errores.xlsx')
+        workbook_nuevo = openpyxl.Workbook()
+        workbook_nuevo.save(file_path_modificado)
+        
+        messagebox.showinfo("Archivo guardado", "El archivo ha sido guardado correctamente.")
+    except Exception as e:
+        messagebox.showerror("Error", f"No se pudo guardar el archivo: {str(e)}")
+
+    # bio.seek(0)
+    # workbook = bio.read()
