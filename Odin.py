5b706799255c88c2f7f331c036e7eb9ee467d8c2
monhabell <monhabell@gmail.com>
2024-08-30 15:35:52 -0500

    odin cambios

--- Odin.py
+++ Odin.py
@@ -196,8 +196,6 @@ def actualizar_aplicacion_sí(ventana_actualizacion, ultima_version):
     vie_actualizaion.title("Actualizar validador")
     vie_actualizaion.geometry("280x100")
     
-    # Agrega el logo de la aplicación
-    vie_actualizaion.wm_iconbitmap(os.path.join(os.path.dirname(sys.executable), "img", "logo.ico"))
 
     # Centra la ventana en la pantalla
     pantalla_ancho = vie_actualizaion.winfo_screenwidth()
